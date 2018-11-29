c2c57b07b35393f62edf047c1cd9a655cd811ce1
Carlos Saiz <orteu15@gmail.com>
2018-11-29 23:08:03 +0100

    Updates

---
+++ precache-manifest.9baa9d89fa836bbf5c7497e50913e24c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f336f4844c600c80c6f8e1c80fb1db0",
+    "url": "/The_Manual_Coach/static/media/background.9f336f48.jpg"
+  },
+  {
+    "revision": "c0010ce3a98243103d4b",
+    "url": "/The_Manual_Coach/static/js/runtime~main.c0010ce3.js"
+  },
+  {
+    "revision": "f89665f47b7089180b48",
+    "url": "/The_Manual_Coach/static/js/main.f89665f4.chunk.js"
+  },
+  {
+    "revision": "d0a3fd0c98844eb0fedd",
+    "url": "/The_Manual_Coach/static/js/1.d0a3fd0c.chunk.js"
+  },
+  {
+    "revision": "f89665f47b7089180b48",
+    "url": "/The_Manual_Coach/static/css/main.fee1a135.chunk.css"
+  },
+  {
+    "revision": "d0a3fd0c98844eb0fedd",
+    "url": "/The_Manual_Coach/static/css/1.67d26aae.chunk.css"
+  },
+  {
+    "revision": "4a37e0059e206dfee59539e8e40a42a0",
+    "url": "/The_Manual_Coach/index.html"
+  }
+];
